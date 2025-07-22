b360129c71f6c0ee9b69018a0e7a27a8cf201a26
SLuvizotto <luvizottodev@gmail.com>
2025-07-21 21:56:11 -0300

    fix: corrigir comentário sobre a transformação em String

--- 04-calc_preco_total.py
+++ 04-calc_preco_total.py
@@ -6,4 +6,4 @@ preco_uni = 12.40
 qtd = 3
 
 print("O preço total da compra de",qtd, "unidades de", nome, "é", str(calcPrecoTotal(preco_uni,qtd)) + ".")
-# transformei em string para que pudesse usar o ponto se o f-string ou deixar um espaço no fim!
+# transformei em string para que pudesse usar o ponto sem o f-string ou deixar um espaço no fim!
