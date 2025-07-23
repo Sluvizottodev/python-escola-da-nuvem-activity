a48423c6c6b67da924cba8458a3b7debf7884ae0
SLuvizotto <luvizottodev@gmail.com>
2025-07-23 12:15:58 -0300

    feat: adicionar cálculo de desconto em produto

---
+++ atividade02-calculadoras/02-calc_desconto.py
@@ -0,0 +1,5 @@
+def calcDesconto(p): return p * 0.8 #0.8 equivale a descontar 20% do original
+nome = "Camiseta"
+preco_ori = 50.00
+
+print("Após o desconto o produto", nome, "custará: R$", calcDesconto(preco_ori))
